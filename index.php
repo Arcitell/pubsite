ac13ca8fac6e8fd22c78bdd5f31abbea07c5b4bf
abhagava <36279633+abhagava@users.noreply.github.com>
2018-08-28 17:45:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,45 @@
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/custom-style.css">
 
+	<script type="text/javascript">__sf_config = {
+customer_id: 97242,
+host: 'www.msgapp.com',
+ip_privacy: 0,
+subsite: '',
+
+__img_path: "/web-next.gif?"
+};
+
+(function() {
+var s = function() {
+var e, t;
+var n = 10;
+var r = 0;
+e = document.createElement("script");
+e.type = "text/javascript";
+e.async = true;
+e.src = "//" + __sf_config.host + "/js/frs-next.js";
+t = document.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(e, t);
+var i = function() {
+if (r < n) {
+r++;
+if (typeof frt !== "undefined") {
+frt(__sf_config);
+} else {
+setTimeout(function() { i(); }, 500);
+}
+}
+};
+i();
+};
+if (window.attachEvent) {
+window.attachEvent("onload", s);
+} else {
+window.addEventListener("load", s, false);
+}
+})();
+</script>
 </head>
 <body>
 	<!-- Modal -->
